7c39af6b14ea0cd90058dda679961ecc2b4d5921
Jeff Tilson <jrtilson@gmail.com>
2012-03-15 12:39:30 -0400

    Initial commit

---
+++ languages/en.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * TGS Contests English Language Translation
+ *
+ * @package TGSContests
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010-2012
+ * @link http://www.thinkglobalschool.org/
+ * 
+ */
+
+$english = array(
+	// Page titles 
+
+	// Labels
+
+	// Other content
+);
+
+add_translation('en',$english);
